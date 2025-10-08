619dd650835d1b27fc0bb64183acdbf718a0794d
José Levi Gonçalves de Lima Menezes <levilima2002@gmail.com>
2025-10-08 17:43:56 -0300

    Criando o pacote componente com a classe AlunoComponente

---
+++ src/main/java/br/bancas/bancasdetcc/Componente/AlunoComponente.java
@@ -0,0 +1,12 @@
+package br.bancas.bancasdetcc.Componente;
+
+public class AlunoComponente {
+    private String mensagem;
+
+    public String getMensagem(){
+        return mensagem;
+    }
+    public void setMensagem(String mensagem){
+        this.mensagem = mensagem;
+    }
+}
